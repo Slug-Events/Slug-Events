4f8598545e4bd6285310256d896a335ddee68b2f
Stanley Good <stanleygvi@gmail.com>
2025-01-21 12:48:05 -0800

    should pass linter now

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,19 @@
+"""Main file for Flask API"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route("/")
 def hello_world():
     """Simple Hello world
 
     Returns:
         String: "Hello World"
     """
-    return 'Hello World'
+    return "Hello World"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()

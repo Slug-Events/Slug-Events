1b6f188a19013813d22726a70ead7a5a4830c3ec
Stanley Good <stanleygvi@gmail.com>
2025-03-03 02:26:08 -0800

    updated config to use env vars from firebase functions

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+export default async () => {
+  const fetch = (await import("node-fetch")).default;
+  
+  // Fetch the environment variables from Firebase
+  const res = await fetch("https://us-central1-slug-events-448506.cloudfunctions.net/getEnv");
+  const firebaseEnv = await res.json();
 
-export default nextConfig;
+  return {
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_BACKEND_URL: firebaseEnv.NEXT_PUBLIC_BACKEND_URL,
+      NEXTAUTH_SECRET: firebaseEnv.NEXTAUTH_SECRET,
+      GOOGLE_CLIENT_ID: firebaseEnv.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: firebaseEnv.GOOGLE_CLIENT_SECRET,
+    },
+  };
+};

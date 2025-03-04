ac4dbdec631b5af4066affa08dd73b13d5373be8
Stanley Good <stanleygvi@gmail.com>
2025-03-04 13:06:51 -0800

    og config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,4 @@
-export default {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "",
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
